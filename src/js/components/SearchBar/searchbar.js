4e76c3e460c1ce394edb7063aab3974c4f010802
Ezequiel Garraffo <egarraffo@gmail.com>
2019-02-07 15:53:23 -0300

    searchbar logea con input

--- src/js/components/SearchBar/searchbar.js
+++ src/js/components/SearchBar/searchbar.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import './searchbar.css';
+import icoLupa from './ic_Search.png';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: '' };
+    this.submitHandler = this.submitHandler.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+
+  handleInput(event) {
+    const target = event.target;
+    this.setState({
+      search: target.value,
+    });
+  }
+
+  submitHandler(event) {
+    alert(this.state.search);
+  }
+
     render() {
       return (
         <div>
-          <input type="text" placeholder="Nunca dejes de buscar"></input>
-          <button type="submit" className="botonBusqueda">Buscar
-          </button>
+          <form onSubmit={this.submitHandler} onChange={this.handleInput}>
+            <input type="text" placeholder="Nunca dejes de buscar"></input>
+            <button type="submit">
+              <img src={icoLupa} alt='' />
+            </button>
+          </form>
         </div>
       );
     }
